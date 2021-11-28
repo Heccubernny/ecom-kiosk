3883f1febdc56a6def2a39c2c397050ecf902735
Ikubanni Paul <ipom4eva@gmail.com>
2021-11-28 19:26:37 +0100

    mail editing

--- eccomerce/settings.py
+++ eccomerce/settings.py
@@ -181,5 +181,5 @@ EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "dailylearners2018@gmail.com"
-EMAIL_HOST_PASSWORD = "G00gl3.c0m"
+EMAIL_HOST_USER = " "
+EMAIL_HOST_PASSWORD =" "
